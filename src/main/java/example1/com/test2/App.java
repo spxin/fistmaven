c2415b4cadf1a0593fe323c61f894e1cc7033998
spxin <15011239398>
2018-09-24 19:36:32 +0800

    新建第一个git项目

---
+++ src/main/java/example1/com/test2/App.java
@@ -0,0 +1,13 @@
+package example1.com.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
